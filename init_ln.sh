432e70bcfcb0866116723dbf043363699e21527a
Kazutoshi Sagi <kazutoshi.sagi@mac.com>
2014-12-16 11:13:21 +0100

    Update init_ln.sh

--- init_ln.sh
+++ init_ln.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
-if [-e $HOME/.bash_profile ]; then
+if [ -f $HOME/.bash_profile ]; then
 	mv $HOME/.bash_profile $HOME/.bash_profile_old
 fi
 ln -s .bash_profile $HOME/.bash_profile
 
-if [-e $HOME/.vimrc ]; then
+if [ -f $HOME/.vimrc ]; then
 	mv $HOME/.vimrc $HOME/.vimrc_old
 fi
 ln -s .vimrc $HOME/.vimrc
 
-if [-e $HOME/.vim ]; then
+if [ -f $HOME/.vim ]; then
 	mv $HOME/.vim $HOME/.vim_old
 fi
 ln -s .vim $HOME/.vim
